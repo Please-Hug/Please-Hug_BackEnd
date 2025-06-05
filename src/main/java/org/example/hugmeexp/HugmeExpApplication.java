769cfecf27980670c2ccd1076e7b03b2c498074c
Roysung0625 <roy06255@gmail.com>
2025-06-05 17:42:17 +0900

    init

---
+++ src/main/java/org/example/hugmeexp/HugmeExpApplication.java
@@ -0,0 +1,13 @@
+package org.example.hugmeexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugmeExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HugmeExpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hugmeexp/HugmeExpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hugmeexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HugmeExpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
