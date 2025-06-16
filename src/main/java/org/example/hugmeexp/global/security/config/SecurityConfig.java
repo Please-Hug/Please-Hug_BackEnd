9238b18cfcea5d0acc62e0a5126b37eee6d2f1b4
Roysung0625 <roy06255@gmail.com>
2025-06-17 03:00:31 +0900

    feat: SwaggerConfig 구현

---
+++ src/main/java/org/example/hugmeexp/global/security/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package org.example.hugmeexp.global.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //@TODO swagger 접속용 임시 securityConfig 추후 수정 필요
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasic -> httpBasic.disable()); // httpBasic 제거 또는 비활성화
+
+        return http.build();
+    }
+}
