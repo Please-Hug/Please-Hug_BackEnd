4a562773f69a4d07deabe9b1abadc708713bad9a
Roysung0625 <roy06255@gmail.com>
2025-06-19 14:41:20 +0900

    feat: Entity 구현

--- src/main/java/org/example/hugmeexp/domain/studydiary/entity/StudyDiaryComment.java
+++ src/main/java/org/example/hugmeexp/domain/studydiary/entity/StudyDiaryComment.java
@@ -1,33 +1,31 @@
 package org.example.hugmeexp.domain.studydiary.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.example.hugmeexp.global.entity.BaseEntity;
 import org.example.hugmeexp.global.entity.User;
 
-import java.util.List;
 
 @Getter
 @Entity
 @Builder
 @RequiredArgsConstructor
-public class StudyDiary extends BaseEntity {
+public class StudyDiaryComment extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="studydiary_id")
+    @Column(name = "studydiary_commment_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studydiary_id")
+    private StudyDiary studyDiary;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "studydiary", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<StudyDiaryComment> comments;
-
-    private String title;
-
-    private String Content;
-
-    private int like;
+    private String content;
 }
--- src/main/java/org/example/hugmeexp/domain/studydiary/entity/StudyDiaryLike.java
+++ src/main/java/org/example/hugmeexp/domain/studydiary/entity/StudyDiaryLike.java
@@ -1,33 +1,28 @@
 package org.example.hugmeexp.domain.studydiary.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.example.hugmeexp.global.entity.BaseEntity;
 import org.example.hugmeexp.global.entity.User;
 
-import java.util.List;
-
-@Getter
 @Entity
-@Builder
+@Getter
 @RequiredArgsConstructor
-public class StudyDiary extends BaseEntity {
+@Builder
+public class StudyDiaryLike extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="studydiary_id")
+    @Column(name = "studydiary_like_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studydiary_id")
+    private StudyDiary studyDiary;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
-    @OneToMany(mappedBy = "studydiary", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<StudyDiaryComment> comments;
-
-    private String title;
-
-    private String Content;
-
-    private int like;
 }
