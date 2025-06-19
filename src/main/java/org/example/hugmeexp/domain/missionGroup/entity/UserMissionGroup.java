132f7ba021ef4a2062216ddfdc36cdeb14b0d54f
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-19 18:22:55 +0900

    feat: 사용자 미션 그룹 엔티티 및 리포지토리 추가 사용자와 미션 그룹 간의 관계를 나타내는 UserMissionGroup 엔티티와 해당 엔티티를 관리하는 UserMissionGroupRepository를 추가했습니다.

---
+++ src/main/java/org/example/hugmeexp/domain/missionGroup/entity/UserMissionGroup.java
@@ -0,0 +1,23 @@
+package org.example.hugmeexp.domain.missionGroup.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.hugmeexp.global.entity.BaseEntity;
+import org.example.hugmeexp.global.entity.User;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class UserMissionGroup extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "mission_group_id", nullable = false)
+    private MissionGroup missionGroup;
+}
