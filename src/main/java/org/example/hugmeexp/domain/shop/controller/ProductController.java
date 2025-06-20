0f0baaf6fd3fb5cb8e9764a83082c88191439872
Taewan Kim <paladyn12@naver.com>
2025-06-20 14:42:22 +0900

    feat: 상품 주문 내역 조회 구현

--- src/main/java/org/example/hugmeexp/domain/shop/controller/ProductController.java
+++ src/main/java/org/example/hugmeexp/domain/shop/controller/ProductController.java
@@ -2,16 +2,20 @@ package org.example.hugmeexp.domain.shop.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.hugmeexp.domain.shop.dto.OrderResponse;
 import org.example.hugmeexp.domain.shop.dto.ProductResponse;
 import org.example.hugmeexp.domain.shop.dto.PurchaseRequest;
 import org.example.hugmeexp.domain.shop.dto.PurchaseResponse;
 import org.example.hugmeexp.domain.shop.service.ProductService;
+import org.example.hugmeexp.domain.user.entity.User;
 import org.example.hugmeexp.global.common.response.Response;
 import org.example.hugmeexp.global.security.CustomUserDetails;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -27,8 +31,12 @@ public class ProductController {
      * @return
      */
     @GetMapping
-    public ResponseEntity<Response<?>> getAllProducts() {
-        List<ProductResponse> allProduct = productService.getAllProducts();
+    public ResponseEntity<Response<?>> getAllProducts(
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        User user = userDetails.getUser();
+        log.info("login user: {}", user.getUsername());
+        List<ProductResponse> allProduct = productService.getAllProducts(user);
         return ResponseEntity.ok().body(Response.builder().data(allProduct).message("Successfully retrieved all products.").build());
     }
 
@@ -51,6 +59,34 @@ public class ProductController {
         String purchaserUsername = userDetails.getUsername();
         PurchaseResponse response = productService.purchase(purchaserUsername, request);
         return ResponseEntity.ok().body(Response.builder().data(response).message("Successfully purchased product.").build());
+    }
+
+    /**
+     * 주문 내역 조회
+     * @param startDate
+     * @param endDate
+     * @param userDetails
+     * @return
+     */
+    @GetMapping("/history")
+    public ResponseEntity<Response<?>> getOrders(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        if (endDate == null) {
+            endDate = LocalDate.now();
+        }
+        if (startDate == null) {
+            startDate = endDate.minusMonths(1);
+        }
+        User user = userDetails.getUser();
+
+        log.info("start date: {}", startDate);
+        log.info("end date: {}", endDate);
+
+        List<OrderResponse> response = productService.getOrders(user, startDate, endDate);
+        return ResponseEntity.ok().body(Response.builder().data(response).message("Successfully retrieved orders.").build());
     }
 
 
