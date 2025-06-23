095d9805f87b1b3b489e5abf30e5a94bc1b5ef5f
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-23 10:43:07 +0900

    refactor: UserSimpleResponse를 UserProfileResponse로 변경 사용자 관련 응답 클래스에서 UserSimpleResponse를 UserProfileResponse로 변경하여 사용자 프로필 정보를 보다 정확하게 반영하도록 수정했습니다.

--- src/main/java/org/example/hugmeexp/domain/missionGroup/dto/response/UserMissionGroupResponse.java
+++ src/main/java/org/example/hugmeexp/domain/missionGroup/dto/response/UserMissionGroupResponse.java
@@ -3,11 +3,11 @@ package org.example.hugmeexp.domain.missionGroup.dto.response;
 import lombok.Builder;
 import lombok.Data;
 
-import org.example.hugmeexp.domain.user.dto.response.UserSimpleResponse;
+import org.example.hugmeexp.domain.user.dto.response.UserProfileResponse;
 @Data
 @Builder(toBuilder = true)
 public class UserMissionGroupResponse {
     private Long id;
-    private UserSimpleResponse user;
+    private UserProfileResponse user;
     private MissionGroupResponse missionGroup;
 }
