0023fbf40b01165a2c1d5998adbb1f77ebde97aa
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-23 11:11:41 +0900

    bufix: UserSimpleResponse를 UserProfileResponse로 변경 됨에 따라 생기는 빌드 오류 해결

--- src/main/java/org/example/hugmeexp/domain/user/dto/response/UserProfileResponse.java
+++ src/main/java/org/example/hugmeexp/domain/user/dto/response/UserProfileResponse.java
@@ -4,9 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+//@AllArgsConstructor
 public class UserProfileResponse {
     private final String profileImage;
     private final String username;
     private final String name;
+    public UserProfileResponse(String username, String name, String profileImage) {
+        this.username = username;
+        this.name = name;
+        this.profileImage = profileImage;
+    }
 }
