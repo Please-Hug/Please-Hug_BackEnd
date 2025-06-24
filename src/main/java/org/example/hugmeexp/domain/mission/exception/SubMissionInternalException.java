639da04be9e609ce993cfd475fef71d52f19d521
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-24 09:05:39 +0900

    fix: SubMissionInternalException 생성자 수정 SubMissionInternalException 클래스에 기본 생성자를 추가하고, 기존 생성자의 매개변수 이름을 변경하여 가독성을 향상시켰습니다.

--- src/main/java/org/example/hugmeexp/domain/mission/exception/SubMissionInternalException.java
+++ src/main/java/org/example/hugmeexp/domain/mission/exception/SubMissionInternalException.java
@@ -4,7 +4,11 @@ import org.example.hugmeexp.global.common.exception.BaseCustomException;
 import org.springframework.http.HttpStatus;
 
 public class SubMissionInternalException extends BaseCustomException {
-    public SubMissionInternalException(String s) {
-        super(HttpStatus.INTERNAL_SERVER_ERROR, s);
+    public SubMissionInternalException(String message) {
+        super(HttpStatus.INTERNAL_SERVER_ERROR, message);
+    }
+
+    public SubMissionInternalException() {
+        super(HttpStatus.INTERNAL_SERVER_ERROR, "서브미션 처리 중 내부 오류가 발생했습니다.");
     }
 }
