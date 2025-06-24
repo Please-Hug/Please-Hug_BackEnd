2d81221ace9046228a8b9a510813a82dc9b44e16
Roysung0625 <roy06255@gmail.com>
2025-06-24 16:16:10 +0900

    chore: import 정리

--- src/main/java/org/example/hugmeexp/domain/studydiary/repository/StudyDiaryLikeRepository.java
+++ src/main/java/org/example/hugmeexp/domain/studydiary/repository/StudyDiaryLikeRepository.java
@@ -1,16 +1,9 @@
 package org.example.hugmeexp.domain.studydiary.repository;
 
-import org.example.hugmeexp.domain.studydiary.entity.StudyDiary;
 import org.example.hugmeexp.domain.studydiary.entity.StudyDiaryLike;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface StudyDiaryLikeRepository extends JpaRepository<StudyDiaryLike, Long> {
