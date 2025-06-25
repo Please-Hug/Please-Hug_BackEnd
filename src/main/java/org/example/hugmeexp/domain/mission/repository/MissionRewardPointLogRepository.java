ff5da931ba94e3a028bcb14a8584f9fe7292c73b
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-25 18:02:52 +0900

    feat: MissionRewardExpLog 및 MissionRewardPointLog 리포지토리에 @Repository 어노테이션 추가 JPA 리포지토리로서의 기능을 명시하기 위해 두 리포지토리에 @Repository 어노테이션을 추가했습니다.

--- src/main/java/org/example/hugmeexp/domain/mission/repository/MissionRewardPointLogRepository.java
+++ src/main/java/org/example/hugmeexp/domain/mission/repository/MissionRewardPointLogRepository.java
@@ -5,11 +5,13 @@ import org.example.hugmeexp.domain.mission.entity.MissionRewardPointLog;
 import org.example.hugmeexp.domain.mission.entity.UserMissionStateLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
+@Repository
 public interface MissionRewardPointLogRepository extends JpaRepository<MissionRewardPointLog, Long> {
     @Query("SELECT m FROM MissionRewardPointLog m WHERE m.userMission.user.id = :userId AND m.createdAt BETWEEN :startDateTime AND :endDateTime")
     List<MissionRewardPointLog> findByUserIdAndCreatedAtBetween(Long userId, LocalDateTime startDateTime, LocalDateTime endDateTime);
