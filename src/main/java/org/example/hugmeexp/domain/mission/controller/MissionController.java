46e54762070c9b32bcf5dcb1372dd81e405ea4b4
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-06-25 17:53:20 +0900

    feat: 사용자 미션 상태 로그 기능 추가 사용자가 미션 상태 변경 이력을 기록할 수 있는 기능을 추가했습니다. 미션 보상 경험치 및 포인트 로그도 함께 저장되도록 구현했습니다.

--- src/main/java/org/example/hugmeexp/domain/mission/controller/MissionController.java
+++ src/main/java/org/example/hugmeexp/domain/mission/controller/MissionController.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/api/v1/missions")
 @RequiredArgsConstructor
---
+++ src/main/java/org/example/hugmeexp/domain/mission/repository/MissionRewardExpLogRepository.java
@@ -0,0 +1,15 @@
+package org.example.hugmeexp.domain.mission.repository;
+
+import org.example.hugmeexp.domain.mission.entity.MissionRewardExpLog;
+import org.example.hugmeexp.domain.mission.entity.UserMissionStateLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+public interface MissionRewardExpLogRepository extends JpaRepository<MissionRewardExpLog, Long> {
+    @Query("SELECT m FROM MissionRewardExpLog m WHERE m.userMission.user.id = :userId AND m.createdAt BETWEEN :startDateTime AND :endDateTime")
+    List<MissionRewardExpLog> findByUserIdAndCreatedAtBetween(Long userId, LocalDateTime startDateTime, LocalDateTime endDateTime);
+}
