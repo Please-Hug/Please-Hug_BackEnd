3dc954d44b6b6c44e3a97835e7d75e4b97e795d9
Taewan Kim <paladyn12@naver.com>
2025-06-26 11:43:29 +0900

    feat: QuestType 추가

--- src/main/java/org/example/hugmeexp/domain/qeust/entity/Quest.java
+++ src/main/java/org/example/hugmeexp/domain/qeust/entity/Quest.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.hugmeexp.domain.qeust.dto.QuestRequest;
+import org.example.hugmeexp.domain.qeust.enums.QuestType;
 
 @Getter
 @Entity
@@ -26,23 +27,29 @@ public class Quest {
     @Column(nullable = false)
     private boolean isDeleted = false;
 
-    public void updateQuest(QuestRequest dto) {
-        this.name = dto.getName();
-        this.url = dto.getUrl();
-    }
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private QuestType type;
 
     @Builder
-    private Quest(String name, String url) {
+    private Quest(String name, String url, QuestType type) {
         this.name = name;
         this.url = url;
+        this.type = type;
     }
 
     // 정적 팩토리 메서드
-    public static Quest createQuest(String name, String url) {
+    public static Quest createQuest(String name, String url, QuestType type) {
         return Quest.builder()
                 .name(name)
                 .url(url)
+                .type(type)
                 .build();
+    }
+
+    public void updateQuest(QuestRequest dto) {
+        this.name = dto.getName();
+        this.url = dto.getUrl();
     }
 
     public void delete() {
