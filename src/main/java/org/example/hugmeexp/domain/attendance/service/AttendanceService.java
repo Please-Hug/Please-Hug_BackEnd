3ee887c433feb7355efd0d09a543a9ef51f2a860
jam9582 <jjam9582@naver.com>
2025-06-29 23:45:45 +0900

    테스트 코드 성공

--- src/main/java/org/example/hugmeexp/domain/attendance/service/AttendanceService.java
+++ src/main/java/org/example/hugmeexp/domain/attendance/service/AttendanceService.java
@@ -11,11 +11,14 @@ import org.example.hugmeexp.domain.attendance.repository.AttendanceRepository;
 import org.example.hugmeexp.domain.user.repository.UserRepository;
 import org.example.hugmeexp.domain.user.entity.User;
 import org.example.hugmeexp.domain.user.service.UserService;
+import org.springframework.cglib.core.Local;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -28,7 +31,6 @@ public class AttendanceService {
     private final UserRepository userRepository;
     private final UserService userService;
 
-
     @Transactional(readOnly = true)
     public List<LocalDate> getAllAttendanceDates(String username) {
 
@@ -40,71 +42,7 @@ public class AttendanceService {
         return attendanceRepository.findDatesByUsername(username);
     }
 
-    // 연속 출석일 계산 메소드
-    private int calculateContinuousDays(String username, LocalDate today) {
-
-        // 과거 1달 출석일 전체 조회
-        LocalDate oneMonthAgo = today.minusDays(29);
-        Set<LocalDate> monthDates = attendanceRepository
-                .findByUser_UsernameAndAttendanceDateBetween(username, oneMonthAgo, today)
-                .stream()
-                .map(Attendance::getAttendanceDate)
-                .collect(Collectors.toSet());
-
-        // 1달동안 연속 출석일 계산
-        int monthContinuous = 0;
-        LocalDate cursor = today;
-        while (monthDates.contains(cursor) && monthContinuous < 30) {
-            monthContinuous++;
-            cursor = cursor.minusDays(1);
-        }
-        if (monthContinuous < 30) {
-            // 1달 미만으로 연속 출석 시 해당 일수 반환
-            return monthContinuous;
-        }
-
-        // 1년 기준으로 출석일 전체 조회
-        LocalDate oneYearAgo = today.minusYears(1);
-        Set<LocalDate> yearDates = attendanceRepository
-                .findByUser_UsernameAndAttendanceDateBetween(username, oneYearAgo, today)
-                .stream()
-                .map(Attendance::getAttendanceDate)
-                .collect(Collectors.toSet());
-
-
-        // 지난 1년동안 연속 출석일 검사
-        int yearContinuous = 0;
-        cursor = today;
-        while (yearDates.contains(cursor) && yearContinuous < 365) {
-            yearContinuous++;
-            cursor = cursor.minusDays(1);
-        }
-        if (yearContinuous < 365) {
-            // 1년 미만으로 연속 출석 시 해당 일수 반환
-            return yearContinuous;
-        }
-        // 1년 이상 연속 출석 시, 가입일 이후의 전체 출석기록 조회
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(AttendanceUserNotFoundException::new);
-        LocalDate fullStartDate = user.getCreatedAt().toLocalDate();
-
-        Set<LocalDate> allDates = attendanceRepository
-                .findByUser_UsernameAndAttendanceDateBetween(username, fullStartDate, today)
-                .stream()
-                .map(Attendance::getAttendanceDate)
-                .collect(Collectors.toSet());
-
-        // 가입일 이후 연속 출석일 계산
-        int fullContinuous = 0;
-        cursor = today;
-        while (allDates.contains(cursor) && fullContinuous < 365) {
-            fullContinuous++;
-            cursor = cursor.minusDays(1);
-        }
-        return fullContinuous;
-    }
-
-    // 출석 상태 조회 (최근 7일, 연속 출석일)
+    // 출석 상태 조회 (일요일~토요일의 일주일, 연속 출석일)
     @Transactional(readOnly = true)
     public AttendanceStatusResponse getAttendanceStatus(String username) {
 
@@ -139,7 +77,83 @@ public class AttendanceService {
         return AttendanceStatusResponse.of(attendanceStatus, continuousDay, today);
     }
 
-        // 출석 체크
+    /* 연속 출석일 계산 메소드, 유저 가입일 null-safe 처리
+    계산 기준은 아래와 같음
+
+     1. 최근 30일 연속
+     2. 30일 이상일 때 최근 1년 연속
+     3. 1년 이상일 때 가입일 이후 전체 연속
+     */
+    private int calculateContinuousDays(String username, LocalDate today) {
+
+        // 과거 1달 출석일 전체 조회
+        LocalDate oneMonthAgo = today.minusDays(29);
+        Set<LocalDate> monthDates = attendanceRepository
+                .findByUser_UsernameAndAttendanceDateBetween(username, oneMonthAgo, today)
+                .stream()
+                .map(Attendance::getAttendanceDate)
+                .collect(Collectors.toSet());
+
+        // 1달동안 연속 출석일 계산
+        int monthContinuous = 0;
+        LocalDate cursor = today;
+        while (monthDates.contains(cursor) && monthContinuous < 30) {
+            monthContinuous++;
+            cursor = cursor.minusDays(1);
+        }
+        if (monthContinuous < 30) {
+            // 1달 미만으로 연속 출석 시 해당 일수 반환
+            return monthContinuous;
+        }
+
+        // 1년 기준으로 출석일 전체 조회
+        LocalDate oneYearAgo = today.minusYears(1);
+        Set<LocalDate> yearDates = attendanceRepository
+                .findByUser_UsernameAndAttendanceDateBetween(username, oneYearAgo, today)
+                .stream()
+                .map(Attendance::getAttendanceDate)
+                .collect(Collectors.toSet());
+
+        // 지난 1년동안 연속 출석일 검사
+        int yearContinuous = 0;
+        cursor = today;
+        while (yearDates.contains(cursor) && yearContinuous < 365) {
+            yearContinuous++;
+            cursor = cursor.minusDays(1);
+        }
+        if (yearContinuous < 365) {
+            // 1년 미만으로 연속 출석 시 해당 일수 반환
+            return yearContinuous;
+        }
+
+        // 유저의 Null-safe 가입일 조회, User.getCreatedAt()이 null이더라도 예외나 NPE가 발생하지 않도록 기본값을 지정해 주는 처리 방식
+        LocalDate userSince = Optional.ofNullable(
+                        userRepository.findByUsername(username)
+                                .orElseThrow(AttendanceUserNotFoundException::new)
+                                .getCreatedAt()
+                )
+                .map(LocalDateTime::toLocalDate)
+                .orElse(LocalDate.MIN);  // 변경: null 시 과거 최저 날짜로 처리
+
+        // 1년 이상 연속 출석 시, 가입일 이후의 전체 출석기록 조회
+        Set<LocalDate> allDates = attendanceRepository
+                .findByUser_UsernameAndAttendanceDateBetween(username, userSince, today)
+                .stream()
+                .map(Attendance::getAttendanceDate)
+                .collect(Collectors.toSet());
+
+        // 가입일 이후 연속 출석일 계산
+        int fullContinuous = 0;
+        cursor = today;
+        while (allDates.contains(cursor) && fullContinuous < 365) {
+            fullContinuous++;
+            cursor = cursor.minusDays(1);
+        }
+        return fullContinuous;
+    }
+
+
+    // 출석 체크
         @Transactional
         public AttendanceCheckResponse checkAttendance (String username) {
 
@@ -157,27 +171,22 @@ public class AttendanceService {
             int exp = 31;
             int point = 1;
 
-            // 신규 출석 저장
-            Attendance attendance = Attendance.of(
-                    user,
-                    today
-            );
-
-            try {
-                // 출석 정보 저장
+            try{
+                // 신규 출석 저장
+                Attendance attendance = Attendance.of(user, today);
                 attendanceRepository.save(attendance);
-            } catch (DataIntegrityViolationException e) {
-                // 중복된 출석 정보 저장 시 예외 처리
+                userService.increaseExp(user, exp);
+                userService.increasePoint(user, point);
+                return AttendanceCheckResponse.builder()
+                        .attend(true)
+                        .exp(exp)
+                        .point(point)
+                        .build();
+            }
+            catch (ObjectOptimisticLockingFailureException lockEX) {
+                // 낙관적 락 충돌 시에도 이미 체크된 것으로 간주
                 throw new AttendanceAlreadyCheckedException();
             }
-
-            userService.increaseExp(user, exp);
-            userService.increasePoint(user, point);
-
-            return AttendanceCheckResponse.builder()
-                    .attend(true)
-                    .exp(exp)
-                    .point(point)
-                    .build();
         }
+
     }
