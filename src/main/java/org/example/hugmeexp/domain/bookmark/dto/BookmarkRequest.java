9895da0df63f9de96a979d45660078318b557470
jam9582 <jjam9582@naver.com>
2025-07-01 07:25:18 +0900

    swagger 추가

--- src/main/java/org/example/hugmeexp/domain/bookmark/dto/BookmarkRequest.java
+++ src/main/java/org/example/hugmeexp/domain/bookmark/dto/BookmarkRequest.java
@@ -1,5 +1,6 @@
 package org.example.hugmeexp.domain.bookmark.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -7,6 +8,10 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class BookmarkRequest {
+
+    @Schema(description = "북마크 이름")
     private String title;
+
+    @Schema(description = "북마크 링크(URL)")
     private String link;
 }
