c3ede17fd3a24a0b7376faace15c0bfc391cdbcd
Taewan Kim <paladyn12@naver.com>
2025-06-30 20:10:14 +0900

    feat: 퀘스트 관련 Swagger 작성

--- src/main/java/org/example/hugmeexp/domain/qeust/controller/QuestAdminController.java
+++ src/main/java/org/example/hugmeexp/domain/qeust/controller/QuestAdminController.java
@@ -1,5 +1,7 @@
 package org.example.hugmeexp.domain.qeust.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.hugmeexp.domain.qeust.dto.QuestRequest;
@@ -16,85 +18,53 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin/quest")
 @Slf4j
+@Tag(name = "Admin - Quest", description = "퀘스트 관련 관리자 API")
 public class QuestAdminController {
 
     private final QuestAdminService questAdminService;
 
-    /**
-     * 퀘스트 생성
-     * @param request
-     * @return
-     */
     @PostMapping
-    public ResponseEntity<Response<?>> createQuest(
-            @RequestBody QuestRequest request) {
+    @Operation(summary = "퀘스트 생성", description = "일일 퀘스트를 생성한다.")
+    public ResponseEntity<Response<?>> createQuest(@RequestBody QuestRequest request) {
         QuestResponse response = questAdminService.createQuest(request);
         return ResponseEntity.status(201).body(Response.builder().data(response).message("Quest successfully created.").build());
     }
 
-    /**
-     * 퀘스트 삭제
-     * @param questId
-     * @return
-     */
     @DeleteMapping("/{questId}")
-    public ResponseEntity<Response<?>> deleteQuest(
-            @PathVariable Long questId) {
+    @Operation(summary = "퀘스트 삭제", description = "일일 퀘스트를 삭제한다.")
+    public ResponseEntity<Response<?>> deleteQuest(@PathVariable Long questId) {
         questAdminService.deleteQuest(questId);
         return ResponseEntity.noContent().build();
     }
 
-    /**
-     * 퀘스트 수정
-     * @param questId
-     * @param request
-     * @return
-     */
     @PutMapping("/{questId}")
-    public ResponseEntity<Response<?>> modifyQuest(
-            @PathVariable Long questId,
-            @RequestBody QuestRequest request) {
+    @Operation(summary = "퀘스트 수정", description = "일일 퀘스트를 수정한다.")
+    public ResponseEntity<Response<?>> modifyQuest(@PathVariable Long questId, @RequestBody QuestRequest request) {
         QuestResponse response = questAdminService.modifyQuest(questId, request);
         return ResponseEntity.ok().body(Response.builder().data(response).message("Quest successfully modified.").build());
     }
 
-    /**
-     * 유저 퀘스트 할당
-     * @param username
-     * @return
-     */
     @PostMapping("/assign/{username}")
-    public ResponseEntity<Response<?>> assignQuest(
-            @PathVariable String username) {
+    @Operation(summary = "유저 퀘스트 할당", description = "사용자에게 생성된 일일 퀘스트를 할당한다.")
+    public ResponseEntity<Response<?>> assignQuest(@PathVariable String username) {
         List<UserQuestResponse> response = questAdminService.assignQuest(username);
         return ResponseEntity.ok(Response.builder().data(response).message("User Quest successfully assigned").build());
     }
 
-    /**
-     * 모든 유저의 퀘스트 진행 상태를 초기화
-     * - 매일 오전 12:00 시에 초기화 작업을 진행하기 위한 기능
-     * @return
-     */
     @PutMapping("/reset")
+    @Operation(summary = "유저 퀘스트 초기화", description = "모든 사용자의 일일 퀘스트의 진행도를 초기화한다.")
     public ResponseEntity<Response<?>> resetQuest() {
         questAdminService.resetQuest();
         return ResponseEntity.noContent().build();
     }
 
-    /**
-     * 퀘스트 일괄 생성
-     * @return
-     */
+
+    // ===== 테스트용 or 사용하지 않는 메서드 =====
+
     @PostMapping("/init")
+    @Operation(summary = "퀘스트 일괄 생성", description = "init.sql로 인해 필요 없어진 호출")
     public ResponseEntity<Response<?>> initQuest() {
         questAdminService.initQuest();
         return ResponseEntity.noContent().build();
-    }
-
-    // ===== 테스트용 =====
-    @GetMapping("/allQuest")
-    public ResponseEntity<Response<?>> findAllQuest() {
-        List<QuestResponse> allQuest = questAdminService.findAllQuest();
-        return ResponseEntity.ok().body(Response.builder().data(allQuest).message("전체 퀘스트 조회").build());
     }
 }
--- src/main/java/org/example/hugmeexp/domain/qeust/controller/QuestController.java
+++ src/main/java/org/example/hugmeexp/domain/qeust/controller/QuestController.java
@@ -1,8 +1,9 @@
 package org.example.hugmeexp.domain.qeust.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.hugmeexp.domain.qeust.dto.QuestResponse;
 import org.example.hugmeexp.domain.qeust.dto.UserQuestResponse;
 import org.example.hugmeexp.domain.qeust.service.QuestService;
 import org.example.hugmeexp.domain.user.entity.User;
@@ -18,16 +19,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/quest")
 @Slf4j
+@Tag(name = "User - Quest", description = "퀘스트 관련 사용자 API")
 public class QuestController {
 
     private final QuestService questService;
 
-    /**
-     * 사용자에게 할당된 모든 퀘스트 조회
-     * @param userDetails
-     * @return
-     */
     @GetMapping
+    @Operation(summary = "사용자 퀘스트 전체 조회", description = "로그인한 유저에게 할당된 일일 퀘스트를 조회한다.")
     public ResponseEntity<Response<?>> getAllQuests(
             @AuthenticationPrincipal CustomUserDetails userDetails) {
         User user = userDetails.getUser();
@@ -35,13 +33,8 @@ public class QuestController {
         return ResponseEntity.ok().body(Response.builder().data(response).message("All Quests successfully retrieved.").build());
     }
 
-    /**
-     * 퀘스트 완료
-     * @param userDetails
-     * @param userQuestId
-     * @return
-     */
     @PutMapping("/complete/{userQuestId}")
+    @Operation(summary = "퀘스트 완료 처리", description = "일일 퀘스트를 완료한다.")
     public ResponseEntity<Response<?>> completeQuest(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long userQuestId) {
