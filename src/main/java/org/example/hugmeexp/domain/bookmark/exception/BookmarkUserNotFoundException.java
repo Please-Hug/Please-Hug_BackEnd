aaca065697f798ccc7bf606615b2b4ada4394669
jam9582 <jjam9582@naver.com>
2025-06-30 14:50:03 +0900

    브랜치 체크아웃 전 커밋

--- src/main/java/org/example/hugmeexp/domain/bookmark/exception/BookmarkUserNotFoundException.java
+++ src/main/java/org/example/hugmeexp/domain/bookmark/exception/BookmarkUserNotFoundException.java
@@ -4,6 +4,7 @@ import org.example.hugmeexp.global.common.exception.BaseCustomException;
 import org.springframework.http.HttpStatus;
 
 public class BookmarkUserNotFoundException extends BaseCustomException{
+
     private static final String MESSAGE = "북마크 사용자를 찾을 수 없습니다.";
     private static final int CODE = 404;
 
