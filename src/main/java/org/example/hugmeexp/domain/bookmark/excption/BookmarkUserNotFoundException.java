3281b1eccb054d1f51c561d47bcaac6b02fbedd7
jam9582 <jjam9582@naver.com>
2025-06-30 11:40:45 +0900

    북마크 CRUD 구현

---
+++ src/main/java/org/example/hugmeexp/domain/bookmark/excption/BookmarkUserNotFoundException.java
@@ -0,0 +1,11 @@
+package org.example.hugmeexp.domain.bookmark.excption;
+
+import org.example.hugmeexp.global.common.exception.BaseCustomException;
+import org.springframework.http.HttpStatus;
+
+public class BookmarkUserNotFoundException extends BaseCustomException{
+    private static final String MESSAGE = "북마크 사용자를 찾을 수 없습니다.";
+    private static final int CODE = 404;
+
+    public BookmarkUserNotFoundException() {super(HttpStatus.NOT_FOUND, MESSAGE, CODE);}
+}
