d5a768403afcb40a243d4afcd6c59d9928d9b480
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-07-03 23:26:09 +0900

    feat: 글로벌 메소드 보안 활성화 WebConfig에서 글로벌 메소드 보안을 활성화하는 설정을 추가했습니다.

--- src/main/java/org/example/hugmeexp/global/common/config/WebConfig.java
+++ src/main/java/org/example/hugmeexp/global/common/config/WebConfig.java
@@ -2,12 +2,15 @@ package org.example.hugmeexp.global.common.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Slf4j
+@EnableMethodSecurity()
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
