7f1a2bdb6166399c9ce359e8fcfe605aad80ee8a
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-07-12 11:24:02 +0900

    feat: 페치 조인 추가

--- src/main/java/org/example/hugmeexp/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/hugmeexp/domain/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package org.example.hugmeexp.domain.user.repository;
 
 import org.example.hugmeexp.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,5 +27,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // phoneNumber를 바탕으로 User 리턴
     Optional<User> findByPhoneNumber(String phoneNumber);
 
+    @Query("SELECT u FROM User u JOIN FETCH u.userMissionGroupList umg JOIN FETCH umg.missionGroup ORDER BY u.exp DESC")
     List<User> findAllByOrderByExpDesc();
 }
